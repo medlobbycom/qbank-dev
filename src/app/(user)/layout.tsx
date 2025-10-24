5e5028825bb925a1b25adb8357c6ce9f2e29330d
Dr Azlan <medlobbycom@gmail.com>
2025-10-24 08:14:29 +0500

    user layout

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -111,7 +111,7 @@ export default function UserLayout({ children }: { children: React.ReactNode }) 
 
 
       {/* Remove min-h-screen to prevent double scrollbar */}
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
 
 
 
